e8660d52c0227083345e41617ea9d0f3414e32fe
Vladimir <vladimirstyopin@mail.ru>
2019-08-25 15:32:19 +0300

    решаем проблему с картинками

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,14 +22,16 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style-min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +39,44 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml())
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () { 
+  return gulp.src([
+    "source/img/**/*.*",
+    "source/fonts/**/*.{woff,woff2}"
+    ], {base: "source"})
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("del", function () {
+  return del("build");
+});
+
+gulp.task("delHtml", function () {
+  return del("build/*.html");
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{jpg,svg}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("start", gulp.series("server"));
+gulp.task("build", gulp.series("del", "copy", "css", "html"));
